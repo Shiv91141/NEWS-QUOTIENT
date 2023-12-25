bfe077cb090298ad3ba9c2133d89ffa7dd609278
Shivam <me210003070@iiti.ac.in>
2023-12-25 18:15:19 +0530

    added navbar and implemented category wise sorting and pagination feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import NewsList from './components/NewsList'
+import Navbar from './components/Navbar';
+import axios from 'axios';
+import Footer from './components/Footer/Footer';
+import NewsContent from './components/NewsContent/NewsContent';
 function App() {
+  const [newsArray, setNewsArray] = useState([]);
+  const [newsResults, setNewsResults] = useState();
+  const [loadMore, setLoadMore] = useState(20);
+  const [category, setCategory] = useState("general");
+
+  console.log(process.env);
+
+  const newsApi = async () => {
+    try {
+      // const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+
+      const news = await axios.get(
+        // `${proxyUrl}https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${loadMore}&category=${category}`
+        `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&pageSize=${loadMore}&category=${category}`
+      );
+      // console.log(news);
+      setNewsArray(news.data.articles);
+      setNewsResults(news.data.totalResults);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    newsApi();
+    // eslint-disable-next-line
+  }, [newsResults, loadMore, category]);
+
   return (
     <div className="App">
-      <NewsList/>
+      <Navbar setCategory={setCategory}/>
+      {/* <NewsList category={category}/> */}
+      {newsResults && (
+        <NewsContent
+          newsArray={newsArray}
+          newsResults={newsResults}
+          loadMore={loadMore}
+          setLoadMore={setLoadMore}
+        />
+      )}
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <span className="name">
+        News Aggregator made by -{" "}
+        <a href="https://www.linkedin.com/in/shivam-jain-93534b246" target="__blank">
+          Shivam Jain
+        </a>
+      </span>
+      <hr style={{ width: "90%" }} />
+      <div className="iconContainer">
+        <a href="https://www.instagram.com/shivamjain4059/" target="__blank">
+          <i className="fab fa-instagram-square fa-2x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/shivam-jain-93534b246" target="__blank">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="#" target="__blank">
+          <i className="fas fa-link fa-2x"></i>
+        </a>
+      </div>
+    </div>
+  )
+}
